46d75e02d0cb5a1dce5f9efa57db907bebb8c78a
Alina Rubart <rubartalina@gmail.com>
2023-08-30 12:42:38 +0200

    Create Form & List components

---
+++ src/components/Form/index.js
@@ -0,0 +1,50 @@
+import "./Form.css";
+
+export function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formElement = form.elements;
+
+    const data = {
+      name: formElement.name.value,
+      isForGoodWeather: formElement.isForGoodWeather.checked,
+    };
+    onAddActivity(data);
+
+    form.reset();
+    formElement.name.focus();
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h1 className="form--headline">Add Activity:</h1>
+      <div>
+        <label className="form--label" htmlFor="name">
+          Name of Activity:{" "}
+        </label>
+        <input
+          className="form--input"
+          name="name"
+          id="name"
+          type="text"
+        ></input>
+      </div>
+      <div>
+        <label className="form--label" htmlFor="isForGoodWeather">
+          Good weather activity:{" "}
+        </label>
+        <input
+          className="form--input"
+          id="isForGoodWeather"
+          name="isForGoodWeather"
+          type="checkbox"
+        ></input>
+      </div>
+      <button className="form--button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
